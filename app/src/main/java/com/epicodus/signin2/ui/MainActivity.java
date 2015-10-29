2fe16649143ef3a8dd31cbaf82e4508f5e54cc0a
the holla <holla@bikefarm.org>
2015-10-29 00:21:24 -0700

    Fix MainActivity not being recognized bug

--- app/src/main/java/com/epicodus/signin2/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/signin2/ui/MainActivity.java
@@ -8,10 +8,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.epicodus.signin2.R;
+import com.epicodus.signin2.models.BikeCollective;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences mPreferences;
-//    private BikeCollective mBikeCollective;
+    private BikeCollective mBikeCollective;
     private TextView mWelcomeText;
     private ImageView mMainImageView;
 
@@ -23,6 +24,29 @@ public class MainActivity extends AppCompatActivity {
         mPreferences = getApplicationContext().getSharedPreferences("signinapp", Context.MODE_PRIVATE);
         mWelcomeText = (TextView) findViewById(R.id.welcomeText);
         mMainImageView = (ImageView) findViewById(R.id.mainImageView);
+
+//        if (!isLoggedIn()) {
+//            Intent intent = new Intent(this, CoopLoginActivity.class);
+//            startActivity(intent);
+//        }
     }
+
+//    private boolean isLoggedIn() {
+//        String username = mPreferences.getString("name", null);
+//        if (username == null) {
+//            return false;
+//        } else {
+//            setBikeCollective(username);
+//            return true;
+//        }
+//    }
+//
+//    private void setBikeCollective(String name) {
+//        BikeCollective bikeCollective = BikeCollective.find(name);
+//        if (bikeCollective != null) {
+//            mBikeCollective = bikeCollective;
+//            mWelcomeText.setText(bikeCollective.getName() + "'s Sign In App");
+//        }
+//    }
 
 }
