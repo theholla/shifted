cf2518dbf4da51c631ac9b810c28f842c3d25335
the holla <holla@bikefarm.org>
2015-11-09 13:01:08 -0800

    Add passing test and fix Robolectric configuration

---
+++ app/src/test/java/models/BikeCollectiveTest.java
@@ -0,0 +1,24 @@
+package models;
+
+import com.epicodus.signin2.models.BikeCollective;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = "src/main/AndroidManifest.xml")
+public class BikeCollectiveTest extends TestCase {
+
+    @Test
+    public void bikeCollectiveInstantiates() throws Exception {
+        BikeCollective bikeCollective = new BikeCollective("Bikerowave", "bike@bike.bike", "123", "Safety!");
+        assertThat(bikeCollective, instanceOf(BikeCollective.class));
+    }
+}
---
+++ app/src/test/java/ui/AdminActivityTest.java
@@ -0,0 +1,9 @@
+package ui;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class AdminActivityTest {
+
+}
