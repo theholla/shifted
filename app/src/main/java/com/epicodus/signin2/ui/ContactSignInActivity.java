8d815d12d94dd50c1d0915d254d3bee63e945dd0
the holla <holla@bikefarm.org>
2015-11-15 15:48:52 -0800

    Dynamically sign user in locally, catch errors, refactor dialogs

--- app/src/main/java/com/epicodus/signin2/ui/ContactSignInActivity.java
+++ app/src/main/java/com/epicodus/signin2/ui/ContactSignInActivity.java
@@ -1,14 +1,14 @@
 package com.epicodus.signin2.ui;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.epicodus.signin2.R;
 import com.epicodus.signin2.models.BikeCollective;
@@ -23,6 +23,8 @@ public class ContactSignInActivity extends AppCompatActivity {
     @Bind(R.id.agreementTextView) TextView mAgreementsTextView;
     @Bind(R.id.nameEditText) EditText mContactName;
     @Bind(R.id.purposeRadioGroup) RadioGroup mPurposeRadioGroup;
+    @Bind(R.id.fixBikeRadioButton) RadioButton mFixBikeRadioButton;
+    @Bind(R.id.volunteerRadioButton) RadioButton mVolunteerRadioButon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,25 +38,32 @@ public class ContactSignInActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 BikeCollective bikeCollective = ActiveBikeCollective.getActiveBikeCollective();
-                String contactName = mContactName.getText().toString().trim();
-                //TODO: grab value using getCheckedRadioButtonId()
-                String contactType = "Volunteer";
+                final String contactName = mContactName.getText().toString().trim();
 
-                String contactPassword = "test";
-                String contactEmail = "test";
-                String contactBirthday = "test";
-
-                SignInEvent newSignInEvent = new SignInEvent(contactName, contactType, bikeCollective);
-                newSignInEvent.save();
-
-                //TODO: make this Toast into a modal
-                Toast toast = Toast.makeText(ContactSignInActivity.this, "Thanks for signing in!", Toast.LENGTH_LONG);
-                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-                toast.show();
-
-                clearFields();
+                if (contactName.isEmpty()) {
+                    showDialog(getString(R.string.dialog_oops), getString(R.string.error_name));
+                } else if (!mFixBikeRadioButton.isChecked() && !mVolunteerRadioButon.isChecked()) {
+                    showDialog(getString(R.string.dialog_oops), getString(R.string.error_choose_purpose));
+                } else {
+                    final int radioChoice = mPurposeRadioGroup.getCheckedRadioButtonId();
+                    RadioButton choice = (RadioButton) findViewById(radioChoice);
+                    String purpose = choice.getText().toString();
+                    SignInEvent newSignInEvent = new SignInEvent(contactName, purpose, bikeCollective);
+                    newSignInEvent.save();
+                    showDialog(getString(R.string.dialog_thanks), getString(R.string.dialog_signedin));
+                    clearFields();
+                }
             }
         });
+    }
+
+    private void showDialog(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ContactSignInActivity.this);
+        builder.setMessage(message)
+                .setTitle(title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create()
+                .show();
     }
 
     public void clearFields() {
